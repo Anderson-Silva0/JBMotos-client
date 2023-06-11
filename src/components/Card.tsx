94534b8a8da30e16e17073ad8c8f07a657a6be46
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-06-11 16:22:04 -0300

    Componente de Card criado

---
+++ src/components/Card.tsx
@@ -0,0 +1,18 @@
+import {ReactNode} from "react"
+import '../styles/card.css'
+
+interface CardProps {
+    children?: ReactNode
+    titulo?: string
+}
+
+export function Card({children, titulo}: CardProps) {
+    return(
+        <div className="card">
+            <h3 className="card-header">{titulo}</h3>
+            <div className="card-body">
+                {children}
+            </div>
+        </div>
+    )
+}
