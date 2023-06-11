d7369b7fa65e30d2d3d131deec185b8e90825c0d
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-06-11 16:19:33 -0300

    fornecedorService com funções de requisição criado

---
+++ src/services/fornecedorService.ts
@@ -0,0 +1,34 @@
+import { ApiService } from "./apiService"
+
+export const FornecedorService = () => {
+    
+    const url = "/fornecedor"
+
+    const salvarFornecedor = (dados: object) => {
+        return ApiService.post(`${url}`, dados)
+    }
+
+    const buscarTodosFornecedores = () => {
+        return ApiService.get(`${url}/buscar-todos`)
+    }
+
+    const buscarFornecedorPorCnpj = (cnpj: string) => {
+        return ApiService.get(`${url}/buscar/${cnpj}`)
+    }
+
+    const atualizarFornecedor = (cnpj: string, dados: object) => {
+        return ApiService.put(`${url}/atualizar/${cnpj}`, dados)
+    }
+
+    const deletarFornecedor = (cnpj: string) => {
+        return ApiService.delete(`${url}/deletar/${cnpj}`)
+    }
+
+    return {
+        salvarFornecedor,
+        buscarTodosFornecedores,
+        buscarFornecedorPorCnpj,
+        atualizarFornecedor,
+        deletarFornecedor
+    }
+}
