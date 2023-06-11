93a77d7eef62ef3994097eb7f31ebf620282c931
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-06-11 16:25:41 -0300

    Componente Form-group criado

---
+++ src/components/Form-group.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react"
+
+interface FormGroupProps {
+    label: string
+    htmlFor: string
+    children?: ReactNode
+}
+
+export function FormGroup({ label, htmlFor, children }: FormGroupProps) {
+
+    return (
+        <div className="form-group">
+            <label htmlFor={htmlFor}>{label}</label>
+            {children}
+        </div>
+    )
+}
