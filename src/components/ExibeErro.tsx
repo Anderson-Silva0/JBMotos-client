6ce5ffbefbadf55ec8966e5c95e8ebde3e6c559e
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-06-11 16:24:13 -0300

    Componente ExibeErro criado

---
+++ src/components/ExibeErro.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+interface Erro {
+  nomeInput: string
+  mensagemErro: string
+}
+
+interface ExibeErroProps {
+  erros: Erro[]
+  nomeInput: string
+}
+
+export function ExibeErro({ erros, nomeInput }: ExibeErroProps) {
+  const erroExistente = erros.find(erro => erro.nomeInput === nomeInput);
+
+  if (erroExistente) {
+    return (
+      <span className="erro">
+        {erroExistente.mensagemErro}
+      </span>
+    );
+  }
+
+  return null;
+}
