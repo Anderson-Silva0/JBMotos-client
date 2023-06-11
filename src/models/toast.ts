a3b7903fe00b8adb48ba24744c3db8f6065275ac
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-06-11 16:09:43 -0300

    Model toast criado

---
+++ src/models/toast.ts
@@ -0,0 +1,38 @@
+import toastr from "toastr"
+
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": true,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": onclick?.prototype,
+  "showDuration": 300,
+  "hideDuration": 1000,
+  "timeOut": 5000,
+  "extendedTimeOut": 1000,
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
+
+export function mostrarMensagem(titulo: string, mensagem: string, tipo: ToastrType) {
+  toastr[tipo](mensagem, titulo)
+}
+
+export function mensagemErro(mensagem: string) {
+  mostrarMensagem('Erro', mensagem, 'error')
+}
+
+export function mensagemSucesso(mensagem: string) {
+  mostrarMensagem('Sucesso', mensagem, 'success')
+}
+
+export function mensagemAlerta(mensagem: string) {
+  mostrarMensagem('Alerta', mensagem, 'warning')
+}
+
+
+
