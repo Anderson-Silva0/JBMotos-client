5009c0c41dd8ee48b0a6ec681ab4bd4737f6314f
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-06-11 16:09:16 -0300

    Model produto criado

---
+++ src/models/produto.ts
@@ -0,0 +1,19 @@
+export interface Produto {
+  id: number
+  nome: string
+  precoCusto: number | string
+  precoVenda: number | string
+  marca: string
+  idEstoque: number
+  cnpjFornecedor: string
+}
+
+export const estadoInicialProduto: Produto = {
+  id: 0,
+  nome: '',
+  precoCusto: 0,
+  precoVenda: 0,
+  marca: '',
+  idEstoque: 0,
+  cnpjFornecedor: ''
+}
