bb5b992c09afbf611df92c4fba6c605400378dfc
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-07-14 23:18:50 -0300

    Atualizar página de listagem de produtos com aprimoramentos no layout e organização do código

--- src/app/produto/listar/page.tsx
+++ src/app/produto/listar/page.tsx
@@ -5,8 +5,10 @@ import { Produto } from "@/models/produto"
 import { mensagemErro } from "@/models/toast"
 import { ProdutoService } from "@/services/produtoService"
 import { useState, useEffect } from "react"
+import imgProduto from '@/images/checklist.png'
+import Image from "next/image"
 
-export default function ListarClientes() {
+export default function ListarProdutos() {
   const [produtos, setProdutos] = useState<Produto[]>([])
 
   const [foiCarregado, setFoiCarregado] = useState<boolean>(false)
@@ -28,37 +30,41 @@ export default function ListarClientes() {
   }, [])
 
   if (!foiCarregado) {
-    return <h1>Carregando...</h1>
+    return <h1 className="carregando">Carregando...</h1>
   }
 
   return (
-    <div>
-      {
-        produtos.length > 1 ? (
-          <h1 className="centered-text">O sistema possui {produtos.length} Produtos cadastrados</h1>
-        ) : produtos.length === 1 ? (
-          <h1 className="centered-text">O sistema possui {produtos.length} Produto cadastrado</h1>
-        ) : produtos.length === 0 && (
-          <h1 className="centered-text">O sistema não possui Produto cadastrado</h1>
-        )
-      }
+    <div className="div-form-container">
+      <h1 className="centered-text">
+        {
+          produtos.length > 1 ? (
+            <>
+              <Image src={imgProduto} width={60} height={60} alt="" /> {produtos.length} Produtos cadastrados
+            </>
+          ) : produtos.length === 1 ? (
+            <>
+              <Image src={imgProduto} width={60} height={60} alt="" /> {produtos.length} Produto cadastrado
+            </>
+          ) : (
+            'Nenhum Produto cadastrado no sistema'
+          )
+        }
+      </h1>
 
-      <div>
-        {produtos.map((produto) => {
-          return (
-            <ProdutoCard
-              key={produto.id}
-              id={produto.id}
-              nome={produto.nome}
-              precoCusto={produto.precoCusto}
-              precoVenda={produto.precoVenda}
-              marca={produto.marca}
-              idEstoque={produto.idEstoque}
-              cnpjFornecedor={produto.cnpjFornecedor}
-            />
-          );
-        })}
-      </div>
+      {produtos.map((produto) => {
+        return (
+          <ProdutoCard
+            key={produto.id}
+            id={produto.id}
+            nome={produto.nome}
+            precoCusto={produto.precoCusto}
+            precoVenda={produto.precoVenda}
+            marca={produto.marca}
+            idEstoque={produto.idEstoque}
+            cnpjFornecedor={produto.cnpjFornecedor}
+          />
+        )
+      })}
     </div>
   );
 }
