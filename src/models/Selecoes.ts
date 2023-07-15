4b4dc74ba6d2b2809bfd835dbb7bec4595c9f8e3
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-07-14 22:43:01 -0300

    Model Selecoes criado

---
+++ src/models/Selecoes.ts
@@ -0,0 +1,9 @@
+export interface OpcoesSelecoes {
+  label: string | number
+  value: string | number
+}
+
+export const estadoInicialOpcoesSelecoes: OpcoesSelecoes = {
+  label: 'Selecione...',
+  value: ''
+}
