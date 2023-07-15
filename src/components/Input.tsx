ec7e2674293bdb1996881a265c09be5acd8a2a26
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-07-14 23:36:12 -0300

    Atualizar o componente Input: adicionar suporte para propriedades de placeholder e type, além de configurar maskChar como null para evitar a exibição de caracteres de máscara no valor do input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,25 +1,55 @@
-import React from "react";
+import React from "react"
 import InputMask from "react-input-mask"
 
 interface inputProps {
     value?: string | number | readonly string[] | undefined
     onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined
+    placeholder?: string
+    type?: string
 }
 
 export const InputCep = (props: inputProps) => (
-    <InputMask mask="99999-999" value={props.value} onChange={props.onChange} />
+    <InputMask
+        mask="99999-999"
+        placeholder={props.placeholder}
+        type={props.type}
+        value={props.value}
+        onChange={props.onChange}
+        maskChar={null}
+    />
 )
 
 export const InputCnpj = (props: inputProps) => (
-    <InputMask mask="99.999.999/9999-99" value={props.value} onChange={props.onChange} />
+    <InputMask
+        mask="99.999.999/9999-99"
+        placeholder={props.placeholder}
+        type={props.type}
+        value={props.value}
+        onChange={props.onChange}
+        maskChar={null}
+    />
 )
 
 export const InputCpf = (props: inputProps) => (
-    <InputMask mask="999.999.999-99" value={props.value} onChange={props.onChange} />
+    <InputMask
+        mask="999.999.999-99"
+        placeholder={props.placeholder}
+        type={props.type}
+        value={props.value}
+        onChange={props.onChange}
+        maskChar={null}
+    />
 )
 
 
 export const InputTelefone = (props: inputProps) => (
-    <InputMask mask="(99) 99999-9999" value={props.value} onChange={props.onChange} />
+    <InputMask
+        mask="(99) 99999-9999"
+        placeholder={props.placeholder}
+        type={props.type}
+        value={props.value}
+        onChange={props.onChange}
+        maskChar={null}
+    />
 )
 
