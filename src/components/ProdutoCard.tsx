66c57c9a0dccc04becda0e9cee1ffc7aaf4264ff
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-07-14 23:29:28 -0300

    Atualizar componente ProdutoCard: ajustes na exibição e formatação dos dados do produto e estoque. Removido função formatCurrency e adicionado import formatarParaReal para formatação do valor em real. Organizado o estilo do componente

--- src/components/ProdutoCard.tsx
+++ src/components/ProdutoCard.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react'
 import { mensagemErro } from '@/models/toast'
 import { Fornecedor, estadoInicialFornecedor } from '@/models/fornecedor'
 import { FornecedorService } from '@/services/fornecedorService'
+import { formatarParaReal } from '@/models/formatadorReal'
 
 export default function ProdutoCard(produto: Produto) {
 
@@ -14,13 +15,6 @@ export default function ProdutoCard(produto: Produto) {
 
   const { buscarEstoquePorId } = EstoqueService()
   const { buscarFornecedorPorCnpj } = FornecedorService()
-
-  const formatCurrency = (value: number | string): string => {
-    return value.toLocaleString('pt-BR', {
-      style: 'currency',
-      currency: 'BRL',
-    })
-  }
 
   useEffect(() => {
     async function buscar() {
@@ -44,31 +38,39 @@ export default function ProdutoCard(produto: Produto) {
       <div className="info-principal">
         <div className='items'>
           <span id="info-title">Produto</span>
-          <strong className="item">Nome: {produto.nome}</strong>
-          <strong className="item">Preço de Custo: {formatCurrency(produto.precoCusto)}</strong>
-          <strong className="item">Preço de Venda: {formatCurrency(produto.precoVenda)}</strong>
-          <strong className="item">Lucro do Produto: {formatCurrency(lucroProduto)}</strong>
-          <strong className="item">Marca: {produto.marca}</strong>
-          <strong className="item">Fornecedor: {fornecedorState.nome}</strong>
+          <div className='div-dados'>Nome</div>
+          <div className='div-resultado'>{produto.nome}</div>
+          <div className='div-dados'>Preço de Custo</div>
+          <div className='div-resultado'>{formatarParaReal(produto.precoCusto)}</div>
+          <div className='div-dados'>Preço de Venda</div>
+          <div className='div-resultado'>{formatarParaReal(produto.precoVenda)}</div>
+          <div className='div-dados'>Lucro do Produto</div>
+          <div className='div-resultado'>{formatarParaReal(lucroProduto)}</div>
+          <div className='div-dados'>Marca</div>
+          <div className='div-resultado'>{produto.marca}</div>
+          <div className='div-dados'>Fornecedor</div>
+          <div className='div-resultado'>{fornecedorState.nome}</div>
         </div>
-        
         <div className='items'>
           <span id="info-title">Estoque</span>
-          <strong className="item">Estoque Mínimo: {estoqueState.estoqueMinimo}</strong>
-          <strong className="item">Estoque Máximo: {estoqueState.estoqueMaximo}</strong>
-          <strong className="item">Quantidade: {estoqueState.quantidade}</strong>
-
-          {estoqueState.status === 'DISPONIVEL' ? (
-            <strong className="item">Status Atual: <span style={{ color: 'green' }}>Disponível</span></strong>
-          ) : estoqueState.status === 'INDISPONIVEL' ? (
-            <strong className="item">Status Atual: <span style={{ color: 'red' }}>Indisponível</span></strong>
-          ) : estoqueState.status === 'ESTOQUE_ALTO' ? (
-            <strong className="item">Status Atual: <span style={{ color: 'orange' }}>Estoque Alto</span></strong>
-          ) : estoqueState.status === 'ESTOQUE_BAIXO' && (
-            <strong className="item">Status Atual: <span style={{ color: 'darkred' }}>Estoque Baixo</span></strong>
-          )}
-          <strong className="ghost-line">.</strong>
-          <strong className="ghost-line">.</strong>
+          <div className='div-dados'>Estoque Mínimo</div>
+          <div className='div-resultado'>{estoqueState.estoqueMinimo}</div>
+          <div className='div-dados'>Estoque Máximo</div>
+          <div className='div-resultado'>{estoqueState.estoqueMaximo}</div>
+          <div className='div-dados'>Quantidade</div>
+          <div className='div-resultado'>{estoqueState.quantidade}</div>
+          <div className='div-dados'>Status Atual</div>
+          <div className='div-resultado'>
+            {estoqueState.status === 'DISPONIVEL' ? (
+              <strong className="item"><span style={{ color: 'green' }}>Disponível</span></strong>
+            ) : estoqueState.status === 'INDISPONIVEL' ? (
+              <strong className="item"><span style={{ color: 'red' }}>Indisponível</span></strong>
+            ) : estoqueState.status === 'ESTOQUE_ALTO' ? (
+              <strong className="item"><span style={{ color: 'orange' }}>Estoque Alto</span></strong>
+            ) : estoqueState.status === 'ESTOQUE_BAIXO' && (
+              <strong className="item"><span style={{ color: 'darkred' }}>Estoque Baixo</span></strong>
+            )}
+          </div>
         </div>
       </div>
     </div>
