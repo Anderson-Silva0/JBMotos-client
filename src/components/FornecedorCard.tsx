212eadf9b56252426edde74e2a1b3f2e630d52f9
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-07-14 23:26:55 -0300

    Atualizar componente FornecedorCard: ajustes na exibição e interação com informações do fornecedor e endereço. Removido import duplicado e organizado o estilo do componente

--- src/components/FornecedorCard.tsx
+++ src/components/FornecedorCard.tsx
@@ -1,12 +1,11 @@
 import { Fornecedor } from '@/models/fornecedor'
 import { Endereco, estadoInicialEndereco } from '../models/endereco'
 import { EnderecoService } from '../services/enderecoService'
-import '../styles/cardListagem.css'
 import { useEffect, useState } from 'react'
 import { mensagemErro } from '@/models/toast'
+import '../styles/cardListagem.css'
 
 export default function FornecedorCard(fornecedor: Fornecedor) {
-
   const [enderecosState, setEnderecoState] = useState<Endereco>(estadoInicialEndereco)
 
   const { buscarEnderecoPorId } = EnderecoService()
@@ -25,28 +24,32 @@ export default function FornecedorCard(fornecedor: Fornecedor) {
 
   return (
     <div className="cardListagem-container">
-
-      <span id="usuario">Fornecedor</span>
-      <div className="info-usuario">
-
-        <strong className="item">CNPJ: {fornecedor.cnpj}</strong>
-        <strong className="item">Nome: {fornecedor.nome}</strong>
-        <strong className="item">Telefone: {fornecedor.telefone}</strong>
-        <strong className="item">
-          Data e Hora de Cadastro: {fornecedor.dataHoraCadastro}
-        </strong>
+      <div className="info-principal">
+        <div className='items'>
+          <span id="info-title">Fornecedor</span>
+          <div className='div-dados'>Nome</div>
+          <div className='div-resultado'>{fornecedor.nome}</div>
+          <div className='div-dados'>CNPJ</div>
+          <div className='div-resultado'>{fornecedor.cnpj}</div>
+          <div className='div-dados'>Telefone</div>
+          <div className='div-resultado'>{fornecedor.telefone}</div>
+          <div className='div-dados'>Data e Hora de Cadastro</div>
+          <div className='div-resultado'>{fornecedor.dataHoraCadastro}</div>
+        </div>
+        <div className='items'>
+          <span id="info-title">Endereço</span>
+          <div className='div-dados'>Endereço</div>
+          <div className='div-resultado'>{enderecosState.rua}</div>
+          <div className='div-dados'>CEP</div>
+          <div className='div-resultado'>{enderecosState.cep}</div>
+          <div className='div-dados'>Número</div>
+          <div className='div-resultado'>{enderecosState.numero}</div>
+          <div className='div-dados'>Bairro</div>
+          <div className='div-resultado'>{enderecosState.bairro}</div>
+          <div className='div-dados'>Cidade</div>
+          <div className='div-resultado'>{enderecosState.cidade}</div>
+        </div>
       </div>
-
-      <span id="endereco">Endereço</span>
-      <div className="info-endereco">
-
-        <strong className="item">Rua: {enderecosState.rua}</strong>
-        <strong className="item">CEP: {enderecosState.cep}</strong>
-        <strong className="item">Número: {enderecosState.numero}</strong>
-        <strong className="item">Bairro: {enderecosState.bairro}</strong>
-        <strong className="item">Cidade: {enderecosState.cidade}</strong>
-      </div>
-
     </div>
   )
 }
