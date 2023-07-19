f2de432dfe4fbc298a9f269dacf098a84c161866
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-07-19 18:54:54 -0300

    Alteração de variável de ambiente

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from "axios";
 
 export const ApiService = axios.create({
-    baseURL: process.env.JBMOTOS_APP_API_BASE_URL,
+    baseURL: process.env.JBMOTOS_APP_API_BASE_URL
 })
