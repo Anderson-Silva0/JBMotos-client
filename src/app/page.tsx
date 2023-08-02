c59901296b10579841100e2e1015d345ce77ad89
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-08-02 00:07:30 -0300

    Adicionado parágrafos na tela Home para teste.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 import '../styles/home.css'
 
-export default function LandingPage() {
+export default function Home() {
   return (
-    <div className='div-form-container'>
-      Aqui será a tela inicial do projeto.
-    </div>
+    <>
+      <div className='div-form-container'>
+        <p>
+          Aqui será a tela inicial do projeto.
+        </p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+      </div>
+    </>
   )
 }
