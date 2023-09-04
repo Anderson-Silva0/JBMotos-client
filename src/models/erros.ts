d02b342a64b8247deb8cfced780fd2bcbfdff1b6
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-09-04 00:25:02 -0300

    Função salvarErros adicionada em erros.ts

--- src/models/erros.ts
+++ src/models/erros.ts
@@ -1,4 +1,21 @@
+import { Dispatch, SetStateAction } from "react"
+
 export interface Erros {
   nomeInput: string
   mensagemErro: string
+}
+
+export const salvarErros = (erro: any, erros: Erros[], setErros: Dispatch<SetStateAction<Erros[]>>) => {
+  const objErro = erro.response.data
+  const keys = Object.keys(objErro)
+  if (!objErro.error && erros.length <= 8) {
+    setErros((errosAntigos) => {
+      const novosErros = keys.map((k) => ({ nomeInput: k, mensagemErro: objErro[k] }))
+      return [...errosAntigos, ...novosErros]
+    })
+  }
+  const erroIgnorado = "Endereço não encontrado para o Id informado."
+  if (objErro.error && objErro.error !== erroIgnorado) {
+    setErros((errosAntigos) => [...errosAntigos, { nomeInput: 'error', mensagemErro: objErro.error }])
+  }
 }
