e2231b93ceeaefc09e06278ea30da70f2b780927
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-10-01 16:55:51 -0300

    Componente ConfirmarDecisao criado

---
+++ src/components/ConfirmarDecisao.tsx
@@ -0,0 +1,29 @@
+import '@/styles/confirmarDecisao.css'
+import { confirmAlert } from 'react-confirm-alert'
+import 'react-confirm-alert/src/react-confirm-alert.css'
+
+export function ConfirmarDecisao(titulo: string, mensagem: string, callback: () => void) {
+    confirmAlert({
+        customUI: ({ onClose }) => (
+            <div className='confirm-custom-ui'>
+                <h1>{titulo}</h1>
+                <p>{mensagem}</p>
+                <button
+                    onClick={() => {
+                        callback()
+                        onClose()
+                    }}
+                >
+                    Sim
+                </button>
+                <button
+                    className="cancel"
+                    onClick={onClose}
+                >
+                    Não
+                </button>
+            </div>
+        ),
+        overlayClassName: 'confirm-overlay'
+    })
+}
