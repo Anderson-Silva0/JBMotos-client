9c73a9c6ea2612ac59bf4092ed34ef6029728a98
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-10-03 15:09:06 -0300

    Criado model formasPagamento.ts que exporta um array com opções de pagamento.

---
+++ src/models/formasPagamento.ts
@@ -0,0 +1,8 @@
+export const formasPagamentos = [
+    { value: 'PIX', label: 'PIX' },
+    { value: 'Transferência Bancária', label: 'Transferência Bancária' },
+    { value: 'Dinheiro', label: 'Dinheiro' },
+    { value: 'Cartão de débito', label: 'Cartão de Débito' },
+    { value: 'Cartão de Crédito', label: 'Cartão de Crédito' },
+    { value: 'Boleto Bancário', label: 'Boleto Bancário' }
+]
