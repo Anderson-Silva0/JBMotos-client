cf3d94e7ab628e9ffbdee5a2560a85660beda5e0
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-10-12 02:35:40 -0300

    Adição de algumas funções para melhorar a reutilização de código

--- src/models/formasPagamento.ts
+++ src/models/formasPagamento.ts
@@ -1,3 +1,5 @@
+import { OpcoesSelecoes } from "./Selecoes"
+
 export const formasPagamentos = [
     { value: 'PIX', label: 'PIX' },
     { value: 'Transferência Bancária', label: 'Transferência Bancária' },
@@ -6,3 +8,26 @@ export const formasPagamentos = [
     { value: 'Cartão de Crédito', label: 'Cartão de Crédito' },
     { value: 'Boleto Bancário', label: 'Boleto Bancário' }
 ]
+
+export const bandeiras: OpcoesSelecoes[] = [
+    { label: "Mastercard", value: "Mastercard" },
+    { label: "Hipercard", value: "Hipercard" },
+    { label: "Visa", value: "Visa" },
+    { label: "Elo", value: "Elo" },
+    { label: "American Express", value: "American Express" },
+    { label: "Sorocred", value: "Sorocred" },
+    { label: "Aura", value: "Aura" },
+    { label: "Diners Club International", value: "Diners Club International" }
+]
+
+export const obterParcelas = () => {
+    const parcelas: OpcoesSelecoes[] = []
+
+    for (let i = 1; i <= 24; i++) {
+        const label = `${i}x`
+        const value = label
+        parcelas.push({ label, value })
+    }
+
+    return parcelas
+}
