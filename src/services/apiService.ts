db36987b3838ad00a90179d42a039bdcd7f66cd7
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2024-08-24 16:18:35 -0300

    added headers

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,5 +1,8 @@
 import axios from "axios"
 
 export const ApiService = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_NGROK_URL + "/api"
+    baseURL: process.env.NEXT_PUBLIC_NGROK_URL + "/api",
+    headers: {
+        'ngrok-skip-browser-warning': 'true'
+    }
 })
