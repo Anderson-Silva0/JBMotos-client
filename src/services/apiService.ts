38e68afeb2f3360df1e36540f2b5b4bbd5f11487
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2024-08-24 15:37:56 -0300

    add enviroment variable ApiService

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const ApiService = axios.create({
-    baseURL: "https://9854-168-194-66-123.ngrok-free.app/api",
+    baseURL: process.env.NGROK_URL + "/api",
     headers: { 
         'ngrok-skip-browser-warning': 'true' 
     }
