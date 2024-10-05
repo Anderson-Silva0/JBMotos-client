5fab197d623982124cd32f5ffde9604bf253fef7
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2024-10-05 00:04:15 -0300

    add ngrok config

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -5,7 +5,7 @@ export const ApiService = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL + "/api",
     headers: {
         'Content-Type': 'application/json',
-        //'ngrok-skip-browser-warning': 'true'
+        'ngrok-skip-browser-warning': 'true'
     }
 })
 
